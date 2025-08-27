4d639de28f2bc70d828e15c3dbc0fb2efab0599d
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2025-08-27 22:19:51 +0300

    Add files via upload

---
+++ Project-E-commerce_Full_Stack-main/src/app/components/product/updateproduct/updateproduct.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../../services/product.service';
+
+
+@Component({
+  selector: 'app-updateproduct',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './updateproduct.component.html',
+  styleUrls: ['./updateproduct.component.css']
+})
+export class UpdateproductComponent implements OnInit {
+  updateForm!: FormGroup;
+  submitted = false;
+  productId!: string;
+  selectedFile: File | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    this.productId = this.route.snapshot.paramMap.get('id')!;
+
+   this.updateForm = this.fb.group({
+  title: ["", [Validators.required, Validators.minLength(3)]],
+  description: ["", [Validators.required, Validators.minLength(5)]],
+  price: ["", [Validators.required, Validators.min(0)]],
+  stock: ["", [Validators.required, Validators.min(0)]]
+});
+
+    
+    this.productService.getProduct(this.productId).subscribe(product => {
+      this.updateForm.patchValue({
+        title: product.name,
+        description: product.description,
+        price: product.price,
+        stock: product.stock
+      });
+    });
+  }
+
+  get formControls() {
+    return this.updateForm.controls;
+  }
+
+  onFileSelect(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpdate() {
+    this.submitted = true;
+
+    if (this.updateForm.valid && this.selectedFile) {
+      const formData = new FormData();
+      formData.append("title", this.updateForm.get("title")!.value);
+      formData.append("description", this.updateForm.get("description")!.value);
+      formData.append("price", this.updateForm.get("price")!.value);
+      formData.append("stock", this.updateForm.get("stock")!.value);
+      formData.append("image", this.selectedFile);
+
+      this.productService.updateProduct(this.productId, formData).subscribe({
+        next: (res) => {
+          console.log("Updated:", res);
+          alert("Product updated successfully!");
+        },
+        error: (err) => console.error("Update error:", err)
+      });
+    }
+  }
+}
